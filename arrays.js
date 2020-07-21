c2c70a2a47c5d2ae8b7172832078d9b53b8c51d4
Terrell Murrell <littlerell12@gmail.com>
2020-07-21 01:26:31 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -11,10 +11,33 @@ function destructivelyAddElementToBeginningOfArray(chocolateBars, candyString) {
   return chocolateBars;
 }
 
-function addElementToEndOfArray() {
-  return [chocolateBars, ...candyString];
+function addElementToEndOfArray(chocolateBars, candyString) {
+  return [...chocolateBars, candyString];
 }
 
-function destructivelyAddElementToEndOfArray() {
+function destructivelyAddElementToEndOfArray(chocolateBars, candyString) {
+  chocolateBars.push(candyString);
+  return chocolateBars;
+}
 
+function accessElementInArray(array, index) {
+  return array[index];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(chocolateBars) {
+  chocolateBars.shift();
+  return chocolateBars;
+}
+
+function removeElementFromBeginningOfArray(chocolateBars) {
+  return chocolateBars.slice(1);
+}
+
+function destructivelyRemoveElementFromEndOfArray(chocolateBars) {
+  chocolateBars.pop();
+  return chocolateBars;
+}
+
+function removeElementFromEndOfArray(chocolateBars) {
+  return chocolateBars.slice(0, chocolateBars.length - 1);
 }
